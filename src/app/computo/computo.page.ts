8f21349c4da8e782781484e04a77950372723466
mastero101 <mastershift_101@hotmail.com>
2023-07-05 21:57:55 -0600

    Create a dinamy star icon class and method

--- src/app/computo/computo.page.ts
+++ src/app/computo/computo.page.ts
@@ -6,10 +6,85 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./computo.page.scss'],
 })
 export class ComputoPage implements OnInit {
+  star1Selected: boolean = false;
+  star2Selected: boolean = false;
+  star3Selected: boolean = false;
+  star4Selected: boolean = false;
+  star5Selected: boolean = false;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  selectStar(starNumber: number) {
+    switch (starNumber) {
+      case 1:
+        this.star1Selected = true;
+        this.star2Selected = false;
+        this.star3Selected = false;
+        this.star4Selected = false;
+        this.star5Selected = false;
+        break;
+      case 2:
+        this.star1Selected = true;
+        this.star2Selected = true;
+        this.star3Selected = false;
+        this.star4Selected = false;
+        this.star5Selected = false;
+        break;
+      case 3:
+        this.star1Selected = true;
+        this.star2Selected = true;
+        this.star3Selected = true;
+        this.star4Selected = false;
+        this.star5Selected = false;
+        break;
+      case 4:
+        this.star1Selected = true;
+        this.star2Selected = true;
+        this.star3Selected = true;
+        this.star4Selected = true;
+        this.star5Selected = false;
+        break;
+      case 5:
+        this.star1Selected = true;
+        this.star2Selected = true;
+        this.star3Selected = true;
+        this.star4Selected = true;
+        this.star5Selected = true;
+        break;
+    }
+  }
+  
+  deselectStar(starNumber: number) {
+    switch (starNumber) {
+      case 1:
+        this.star1Selected = false;
+        this.star2Selected = false;
+        this.star3Selected = false;
+        this.star4Selected = false;
+        this.star5Selected = false;
+        break;
+      case 2:
+        this.star2Selected = false;
+        this.star3Selected = false;
+        this.star4Selected = false;
+        this.star5Selected = false;
+        break;
+      case 3:
+        this.star3Selected = false;
+        this.star4Selected = false;
+        this.star5Selected = false;
+        break;
+      case 4:
+        this.star4Selected = false;
+        this.star5Selected = false;
+        break;
+      case 5:
+        this.star5Selected = false;
+        break;
+    }
+  }
+  
 }
