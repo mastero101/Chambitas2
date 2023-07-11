994b9ea1e054cbf556be38015414ba105c5e8308
mastero101 <mastershift_101@hotmail.com>
2023-07-11 01:34:35 -0600

    Add functions to ion-icons
    function copy direction
    function export to google maps
    function to show more elements on HorarioServicio

--- src/app/profile/profile.page.ts
+++ src/app/profile/profile.page.ts
@@ -6,10 +6,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.page.scss'],
 })
 export class ProfilePage implements OnInit {
+  direccion: string = "Av.Fundadores 4 Campeche,Campeche 24014";
+  cardVisible: boolean = false;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  copiarTexto() {
+    const tempTextArea = document.createElement('textarea');
+    tempTextArea.value = this.direccion;
+
+    document.body.appendChild(tempTextArea);
+
+    tempTextArea.select();
+    document.execCommand('copy');
+
+    document.body.removeChild(tempTextArea);
+
+    alert('Texto copiado');
+  }
+
+  openGoogleMaps() {
+    const address = this.direccion;
+    const encodedAddress = encodeURIComponent(address);
+    const url = `https://www.google.com/maps/search/?api=1&query=${encodedAddress}`;
+  
+    window.open(url, '_system');
+  }
+
+  toggleCard() {
+    this.cardVisible = !this.cardVisible;
+  }
 }
