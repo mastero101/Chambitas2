91a0cc1733ebdda793da632080e520963e0e2279
mastero101 <mastershift_101@hotmail.com>
2023-07-17 13:34:28 -0600

    New package for Twillio
    Add FormsModule and ReactiveForms
    
    message.page changes to implements Forms
    
    Constructos changes to capture Forms and Validators
    
    new method for Twillio Whatsapp message sender

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,11 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { NavBarPageModule } from './nav-bar/nav-bar.module';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, NavBarPageModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, NavBarPageModule, FormsModule, ReactiveFormsModule],
   exports: [],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
--- src/app/message/message.page.ts
+++ src/app/message/message.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+import axios from 'axios';
 
 @Component({
   selector: 'app-message',
@@ -6,10 +9,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./message.page.scss'],
 })
 export class MessagePage implements OnInit {
+  msgForm: FormGroup;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private formBuilder: FormBuilder) {
+    this.msgForm = this.formBuilder.group({
+      asunto: ['', Validators.required],
+      mensaje: ['', Validators.required]
+    });
   }
 
+  ngOnInit() {
+    this.msgForm = this.formBuilder.group({
+      asunto: ['', Validators.required],
+      mensaje: ['', Validators.required]
+    });
+  }
+
+  async whatsapp() {
+    if (this.msgForm.valid) {
+      const asunto = this.msgForm.get('asunto')?.value;
+      const mensaje = this.msgForm.get('mensaje')?.value;
+      console.log(asunto);
+      console.log(mensaje);
+
+      // Enviar mensaje utilizando Axios o la biblioteca de tu elección
+      try {
+        const response = await axios.post(
+          'https://api.twilio.com/2010-04-01/Accounts/AC100cb9d640b55673e2b655f9d0229498/Messages.json',
+          new URLSearchParams({
+            'To': 'whatsapp:+5219811402316',
+            'From': 'whatsapp:+14155238886',
+            'Body': `${asunto} - ${mensaje}`
+          }),
+          {
+            auth: {
+              username: 'AC100cb9d640b55673e2b655f9d0229498',
+              password: '8270471c60f3ca15e7157fec48ce8a8c'
+            }
+          }
+        );
+        console.log(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    } else {
+      console.log('Por favor, completa los campos requeridos.');
+    }
+  }
 }
