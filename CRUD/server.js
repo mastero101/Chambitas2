5a8f54bab55fea8fe67e5f600f0b650c6c15a4cf
mastero101 <mastershift_101@hotmail.com>
2023-07-20 06:44:54 -0600

    update CRUD for app.get /ordenes

--- CRUD/server.js
+++ CRUD/server.js
@@ -65,6 +65,19 @@ app.get('/apikey', (req, res) => {
     });
 });
 
+app.get('/ordenes', (req, res) => {
+    const sql = 'SELECT * FROM ordenes';
+
+    connection.query(sql, (error, results) => {
+        if (error) throw error;
+        if (results.length > 0) {
+            res.json(results);
+        } else {
+            res.send('No results');
+        }
+    });
+});
+
 app.post('/add', (req, res) => {
     const sql = 'INSERT INTO afiliados SET ?';
 
